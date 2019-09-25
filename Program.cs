6a3bad472040966905b4d41f3e6c7dfa4f8a5607
Alexander <alex.esbj@gmail.com>
2019-09-25 16:30:39 +0200

    Skapar filer
    lade till grunderna för att kunna använda det jag vill

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Väderstationen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
