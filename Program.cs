f583b6d062a4cf59e81b4683acc961225fee4ea2
Alexander <alex.esbj@gmail.com>
2019-09-25 18:03:30 +0200

    Grunderna på lägg till temp & UX design

--- Program.cs
+++ Program.cs
@@ -6,14 +6,28 @@ namespace Väderstationen
     {
         static void menu()
         {
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine("                Vad vill du göra?");
+            Console.WriteLine("");
             Console.WriteLine("[L]ägg till tempratur");
             Console.WriteLine("[S]kriv ut alla tempraturer & medeltemraturen");
             Console.WriteLine("[T]a bort ett värde");
             Console.WriteLine("[A]vsluta");
+            Console.WriteLine("--------------------------------------------------");
         }
+        List<int> Väderdata = new List<int>();
         static void add()
         {
-
+            Console.WriteLine("Skriv in tempraturen");
+            string sTemp = Console.ReadLine();
+            try
+            {
+                int temp = Convert.ToInt32(sTemp);
+            }
+            catch
+            {
+                Console.WriteLine("Du matade inte in en tempratur i siffervärde.");
+            }
         }
         static void writeMedian()
         {
@@ -25,25 +39,31 @@ namespace Väderstationen
         }
         static void Main(string[] args)
         {
+            Console.WriteLine("");
             Console.WriteLine("Hej och välkommen till Väderstationen!");
-            Console.WriteLine("Vad vill du göra idag?");
-            menu();
-            string input = Console.ReadLine().ToUpper();
-            char choise = input[0];
-            Console.WriteLine(choise);
-            switch (choise)
+            Console.WriteLine("");
+            while (true)
             {
-                case 'L':
-                    add();
-                    break;
-                case 'S':
-                    writeMedian();
-                    break;
-                case 'T':
-                    taBort();
-                    break;
-                case 'A':
-                    return;
+                menu();
+                string input = Console.ReadLine().ToUpper();
+                char choise = input[0];
+                switch (choise)
+                {
+                    case 'L':
+                        add();
+                        break;
+                    case 'S':
+                        writeMedian();
+                        break;
+                    case 'T':
+                        taBort();
+                        break;
+                    case 'A':
+                        return;
+                    default:
+                        Console.WriteLine("Jag förstod inte vad du skrev.");
+                        break;
+                }
             }
         }
     }
