06a03b43dc17eb1dc9075ed97b96492e157100eb
Alexander <alex.esbj@gmail.com>
2019-10-01 16:04:40 +0200

    Struktur och kommentarer
    Kommenterar upp koden lite bättre och strukturerar om val till funktioner

--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@ namespace Väderstationen
 {
     class Program
     {
-        static void menu()
+        static List<int> weatherData = new List<int>();
+        static void printMenu()
         {
             Console.WriteLine("--------------------------------------------------");
             Console.WriteLine("                Vad vill du göra?");
@@ -15,7 +16,7 @@ namespace Väderstationen
             Console.WriteLine("[A]vsluta");
             Console.WriteLine("--------------------------------------------------");
         }
-        static int countMedian(List<int> input)
+        static int printAll_CountMedian(List<int> input)
         {
             if (input.Count > 0)
             {
@@ -24,7 +25,7 @@ namespace Väderstationen
                 for (int i = 0; i < input.Count; i++)
                 {
                     countTemp = countTemp + input[i];
-                    System.Console.WriteLine("Tempratur "+(i+1) + ": " + input[i]);
+                    System.Console.WriteLine("Tempratur " + (i + 1) + ": " + input[i]);
                 }
                 medianTemp = countTemp / input.Count;
                 return medianTemp;
@@ -33,18 +34,54 @@ namespace Väderstationen
             {
                 System.Console.WriteLine("Det finns inga lagrade mätningar.");
                 return 0;
-                
             }
+        }
+        static void addValue()
+        {
+            string sTemp = Console.ReadLine();
+            try
+            {
+                int temp = Convert.ToInt32(sTemp);
+                weatherData.Add(temp);
+            }
+            catch
+            {
+                Console.WriteLine("Du matade inte in en tempratur i siffervärde.");
+            }
+            return;
+
+        }
+        static void removeValue()
+        {
+            if (weatherData.Count > 0)
+            {
+                printAll_CountMedian(weatherData);
+                Console.WriteLine("Ange vilken mätning du vill ta bort.");
+                try
+                {
+
+                    int toRemove = Convert.ToInt32(Console.ReadLine());
+                    weatherData.RemoveAt(toRemove - 1);
+                }
+                catch
+                {
+                    Console.WriteLine("Ange indexplatsen på tepmraturen i siffervärde.");
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Det finns ingen väderdata lagrad.");
+            }
+            return;
         }
         static void Main(string[] args)
         {
-            List<int> weatherData = new List<int>();
 
-            weatherData.Add(1);
-            weatherData.Add(2);
-            weatherData.Add(3);
-            
-            
+            //Startvärden för att underlätta felsökning.
+            // weatherData.Add(23);
+            // weatherData.Add(-10);
+            // weatherData.Add(14);
 
             Console.WriteLine("");
             Console.WriteLine("Hej och välkommen till Väderstationen!");
@@ -52,47 +89,27 @@ namespace Väderstationen
 
             while (true)
             {
-                menu();
+                printMenu();
+                //Läser användare input och tar första bokstaven, gör om till ngt programmet förstår.
+                //Användaren kan endast mata in bokstäver
                 string input = Console.ReadLine().ToUpper();
                 char choise = input[0];
+
+                //Hanterar val och kallar funktioner för varige val
                 switch (choise)
                 {
-                    case 'L'://lägg till
+                    case 'L'://lägg till temp
                         Console.WriteLine("Skriv in tempraturen");
-                        string sTemp = Console.ReadLine();
-                        try
-                        {
-                            int temp = Convert.ToInt32(sTemp);
-                            weatherData.Add(temp);
-                        }
-                    catch
-                        {
-                            Console.WriteLine("Du matade inte in en tempratur i siffervärde.");
-                        }
+                        addValue();
                         break;
-                    case 'S'://skriv ut data och medel
-
-                        int medelvärde = countMedian(weatherData);
-                        System.Console.WriteLine("Medelvärdet är: " + medelvärde);
+                    case 'S'://skriv ut alla temp och medeltemp
+                        int medelvärde = printAll_CountMedian(weatherData);
+                        System.Console.WriteLine("Medeltempraturen är: " + medelvärde);
                         break;
-                    case 'T'://ta bort
-                        if (weatherData.Count > 0){
-                            countMedian(weatherData);
-                            Console.WriteLine("Ange vilken mätning du vill ta bort.");
-                            try{
-
-                            int toRemove = Convert.ToInt32(Console.ReadLine());
-                            weatherData.RemoveAt(toRemove-1);
-                            break;
-                            }catch{
-                                Console.WriteLine("Ange indexplatsen på tepmraturen i siffervärde.");
-                                break;
-                            }
-                        }else{
-                            Console.WriteLine("Det finns ingen väderdata lagrad.");
-                            break;
-                        }
-                    case 'A'://avsluta
+                    case 'T'://ta bort temp
+                        removeValue();
+                        break;
+                    case 'A'://avsluta programmet
                         return;
                     default:
                         Console.WriteLine("Jag förstod inte vad du skrev.");
